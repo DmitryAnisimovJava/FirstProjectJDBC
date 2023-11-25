2b73373514453eda7ad6c245f544137d541bc4d4
DmitryAnisimovJava <DmitryAnisimovJava@users.noreply.github.com>
2023-11-25 10:24:58 +0300

    ConnectionManager and application.properties file

--- src/main/java/com/anisimov/jdbc/starter/JdbcRunner.java
+++ src/main/java/com/anisimov/jdbc/starter/JdbcRunner.java
@@ -1,20 +1,17 @@
 package com.anisimov.jdbc.starter;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import org.postgresql.Driver;
+
+import com.anisimov.jdbc.starter.util.ConnectionManager;
 
 public class JdbcRunner {
 
 	public static void main(String[] args) throws SQLException {
 		Class<Driver> driverClass = Driver.class;
-		String username = "admin";
-		String password = "password";
-		String urlString = "jdbc:postgresql://localhost:5432/flight_repository";
-		try (Connection connector = DriverManager.getConnection(urlString, username, password)) {
-			System.out.println(connector.getTransactionIsolation());
+		try (var connection = ConnectionManager.open()) {
+			System.out.println(connection.getTransactionIsolation());
 		}
 
 	}
